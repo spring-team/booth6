79a61257deb80f6e36bc226e9ca8a4cabba69704
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 16:01:08 +0000

    Initial commit from Atomist

---
+++ src/main/java/ing/Booth6Application.java
@@ -0,0 +1,12 @@
+package ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booth6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booth6Application.class, args);
+	}
+}
